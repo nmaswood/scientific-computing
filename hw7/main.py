3cb7dcb760943086998456ccf4882f81c4363d96
Nasr Maswood <nmaswood@uchicago.edu>
2016-11-27 19:16:11 -0600

    asdfjasdf

--- hw7/main.py
+++ hw7/main.py
@@ -9,12 +9,19 @@ from scipy.sparse import csgraph
 # http://www.math.iit.edu/~fass/477577_Chapter_16.pdf
 # http://georgioudakis.com/blog/categories/python/cg.html
 
+"""
+Explanation:
+
+For the first part I implemented the iterative CG solver. Once I had that working
+I adapted for use with the Laplacian Solver. You can check the original solver works by
+running the program and seeing that its solution is equivalent to scipy's answer.
+
+"""
+
+# Part 1
+
+
 class ConjugateGradientBasic():
-
-  def __init__(self, tolerance, error):
-
-    self.tolerance = tolerance
-    self.error = error
 
   def solve(self, A,B, X):
 
@@ -37,29 +44,28 @@ class ConjugateGradientBasic():
 
     return X
 
-x  = np.array([
-  [4,1],
-  [1,3]
-])
+class TestConjugateGradientBasic():
 
-y  = np.array(
-  [1,2]
-)
+  def test(self):
 
-res = cg(x,y)
-#print (res[0])
+    x  = np.array([
+      [4,1],
+      [1,3]
+    ])
 
-r = ConjugateGradientBasic(tolerance = 1e-6, error = 10)
+    y  = np.array(
+      [1,2]
+    )
 
-res = r.solve(x,y, np.zeros(x.shape[0]))
-######################################################################################################
+    resSCIPY = cg(x,y)[0]
+
+    resMINE = ConjugateGradientBasic().solve(x,y, np.zeros(x.shape[0]))
+
+    print ("The Scipy implementation of CG and my implemetation are the same: {}".format(np.array_equal(resSCIPY, resMINE)))
+
+# Part 2
 
 class ConjugateGradientLapalcian():
-
-  def __init__(self, tolerance, error):
-
-    self.tolerance = tolerance
-    self.error = error
 
   def solve(self, A,B, X, steps):
 
@@ -70,91 +76,76 @@ class ConjugateGradientLapalcian():
 
     for _ in range(steps):
 
-      r_DOT_r = np.dot(r,r)
-      A_DOT_pi1 = np.dot(A,pi1)
+      r_DOT_r = np.inner(r,r)
+      A_DOT_pi1 = np.inner(A,pi1)
 
-      alpha =  r_DOT_r / np.dot(pi1,A_DOT_pi1)
-      X += np.dot(alpha, pi1)
-      r1 = r - np.dot(alpha,A_DOT_pi1)
-      beta =  np.dot(r1,r1) / r_DOT_r
+      alpha =  r_DOT_r / np.inner(pi1,A_DOT_pi1)
+      X += np.inner(alpha, pi1)
+      r1 = r - np.inner(alpha,A_DOT_pi1)
+      beta =  np.inner(r1,r1) / r_DOT_r
       pi1 = r1 + beta * pi1
       r = r1
 
     return X
 
-def sinsin(x,y):
-  return sin(pi*2*x)*sin(pi*3*y);
 
-def bumpbump(x,y):
-  return -2*(x*(1-x)+y*(1-y));
+class ConjugateGradientLapalcianTest():
 
-def zero(x,y):
-  return 0
+  def test(self):
 
-def fill(u, f, dx):
-  rows, cols = u.shape
+      def sinsin(x,y):
+        return sin(pi*2*x)*sin(pi*3*y);
 
-  for i  in range(rows):
-    u[i][0] = u[i][rows-1] = u[0][i] = u[N-1][i] = 0.0
+      def bumpbump(x,y):
+        return -2*(x*(1-x)+y*(1-y));
 
-  for  i in range(rows):
-    for j in range(cols):
-      u[i][j] = f(i * dx, j * dx)
+      def zero(x,y):
+        return 0
 
-def Cu(u):
-  rows, cols = u.shape
+      def fill(u, f, dx):
+        rows, cols = u.shape
 
-  retMatrix = np.zeros((rows,cols))
+        for i  in range(rows):
+          u[i][0] = u[i][rows-1] = u[0][i] = u[N-1][i] = 0.0
 
-  for i in range(1,rows-1):
-    for j in range(1,cols-1):
-      retMatrix[i][j] = sqrt(N-1) * (4 * u[i][j] \
-        - u[i-1][j] - u[i+1][j] - u[i][j-1] - u[i][j+1])
+        for  i in range(rows):
+          for j in range(cols):
+            u[i][j] = f(i * dx, j * dx)
 
-  return retMatrix
+      def Cu(u):
+
+        rows, cols = u.shape
+
+        retMatrix = np.zeros((rows,cols))
+
+        for i in range(1,rows-1):
+          for j in range(1,cols-1):
+            retMatrix[i][j] = sqrt(N-1) * (4 * u[i][j] \
+              - u[i-1][j] - u[i+1][j] - u[i][j-1] - u[i][j+1])
+
+        return retMatrix
+
+      N = 10
+      u = np.zeros((N,N))
+      r = np.zeros((N,N))
+
+      dx = 1.0 / (N - 1)
+
+      fill(r, sinsin,dx)
+
+      CuMatrix = Cu(r)
+
+      solver = ConjugateGradientLapalcian()
+
+      fill(r, bumpbump, dx)
+
+      s = r
+
+      result = solver.solve(s, CuMatrix, r,  steps = N)
+
+      print (result)
 
 if __name__ == "__main__":
-  print ("bar")
-  N = 10
-  u = np.zeros((N,N))
-  r = np.zeros((N,N))
-  dx = 1.0 / (N - 1)
-  fill(r, sinsin,dx)
 
-  normSquared = norm(r)
-  print (normSquared)
-
-  CuMatrix = Cu(r)
-  print (CuMatrix)
-
-  # Matrix s(r)
-
-  output1 = "Test case 1: The initial norm squared {}".format(normSquared)
-
-  fill(r, bumpbump, dx)
-
-  s = r
-
-  normRessq = norm(r)
-
-  output2 = "Test case 2: The initial norm squared of the residual is {}".format(normRessq)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print (csgraph.laplacian(x, normed=False))
-
-
-
+  TestConjugateGradientBasic().test()
+  ConjugateGradientLapalcianTest().test()
